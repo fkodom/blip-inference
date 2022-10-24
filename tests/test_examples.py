4dd943a3efb8f085c64a8c5d4239a2fba3ff745a
Frank Odom <frank.odom.iii@gmail.com>
2022-10-24 16:00:07 +0000

    Improve test coverage; remove print statements from test examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,7 +7,6 @@ import blip
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-@pytest.fixture(params=["base", "large"])
 @pytest.mark.parametrize("name", ["base", "large"])
 def test_usage(name: str):
     model, preprocess = blip.load(name, device=device)
@@ -18,12 +17,7 @@ def test_usage(name: str):
 
     with torch.no_grad():
         logits_per_image, _ = model(image, text)
-    probs = torch.softmax(logits_per_image, dim=-1)
-
-    print("\nPredictions:\n")
-    for idx, value in enumerate(probs.squeeze()):
-        print(f"{raw_text[idx]:>16s}: {100 * value.item():.2f}%")
-    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+    _ = torch.softmax(logits_per_image, dim=-1)
 
 
 @pytest.mark.parametrize("name", ["base", "large"])
@@ -40,11 +34,24 @@ def test_zero_shot(name: str):
 
     image_features /= image_features.norm(dim=-1, keepdim=True)
     text_features /= text_features.norm(dim=-1, keepdim=True)
-    similarity = (100 * image_features @ text_features.T).softmax(dim=-1)
+    _ = (100 * image_features @ text_features.T).softmax(dim=-1)
 
-    print("\nPredictions:\n")
-    for idx, value in enumerate(similarity.squeeze()):
-        print(f"{raw_text[idx]:>16s}: {100 * value.item():.2f}%")
+    _, _ = model(image, text)
+
+
+def test_feature_extractor():
+    model, preprocess = blip.load("feature_extractor", device)
+
+    raw_text = ["a diagram", "a dog", "a cat"]
+    text = blip.tokenize(raw_text).to(device)
+    image = preprocess(Image.open("kitten.jpeg")).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        image_features = model.encode_image(image)
+        text_features = model.encode_text(text)
+
+    image_features /= image_features.norm(dim=-1, keepdim=True)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
 
 
 def test_available_models():
